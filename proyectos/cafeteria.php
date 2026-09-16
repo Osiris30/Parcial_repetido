0f907d6a59e11b7de3b650c3a509dc59fd4b0b11
Osiris <andradeivania16@gmail.com>
2026-09-16 11:40:20 -0600

    creando arreglo y creando la carpeta imagenes y agregando imagenes

---
+++ proyectos/cafeteria.php
@@ -0,0 +1,67 @@
+<?php
+
+$CATEGORIAS=[
+    "BEBIDAS",
+    "POSTRES",
+    "ALIMENTOS"
+
+];
+
+$BEBIDAS=[
+    ["nombre"=>"COCA COLA",
+    "precio"=>1.35
+
+    ],
+
+    ["nombre"=> "Jugo de Naranja",
+    "precio"=>2.50
+    ],
+    ["nombre"=> "Limonada",
+    "precio"=>3.00
+    ],
+    ["nombre"=> "Jugo de ciruela",
+    "precio"=>2.80
+    ],
+
+];
+
+$POSTRES=[
+    ["nombre"=> "TRES LECHES",
+    "precio"=>1.75],
+
+     ["nombre"=> "CUPCAKES",
+    "precio"=>2.0],
+
+     ["nombre"=> "PASTEL ",
+    "precio"=>1.75],
+
+     ["nombre"=> "BROWNIES",
+    "precio"=>3.5],
+];
+
+$ALIMENTOS=[
+     ["nombre"=> "CARNE ASADA",
+    "precio"=>3.5],
+     ["nombre"=> "POLLO A LA PLANCHA",
+    "precio"=>4.5],
+     ["nombre"=> "PESCADO",
+    "precio"=>10.00],
+
+];
+
+$error=[];
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
